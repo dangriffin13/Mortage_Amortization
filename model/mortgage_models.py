a8fb978abe23eb1b8300b522c86d613516f27a9a
Dan Griffin <dangriffin13@gmail.com>
2017-07-10 01:46:00 -0400

    starting formulas

--- model/mortgage_models.py
+++ model/mortgage_models.py
@@ -1,6 +1,7 @@
 
 class MthAmort:
-	def __init__(self, 
+	def __init__(self,
+				payment_period, 
 				payment,
 				principal_payment,
 				interest_payment,
@@ -9,6 +10,7 @@ class MthAmort:
 				cumulative_interest_payment,
 				cumulative_total_payment
 				):
+		self.period = payment_period
 		self.pmt = payment
 		self.prin_pmt = principal_payment
 		self.int_pmt = interest_payment
@@ -16,3 +18,32 @@ class MthAmort:
 		self.prin_cum = cumulative_principal_payment
 		self.int_cum = cumulative_interest_payment
 		self.pmt_cum = cumulative_total_payment
+
+
+def payment_size(principal, rate, total_payments):
+	p = principal
+	r = rate
+	n = total_payments
+
+	pmt = r * p / (1 - (1 + r))
+
+
+def interest_size(remaining_balance, payment, rate):
+	pass
+
+
+def principal_paydown(remaining_balance, payment, rate):
+	pass
+
+
+def recalc_principal(remaining_balance, payment):
+	pass
+
+def recalc_cum_prin():
+	pass
+
+def recalc_cum_int():
+	pass
+
+def recalc_cum_pmt():
+	pass
