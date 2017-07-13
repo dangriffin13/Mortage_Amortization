3d6f9c466c354e439477765a05554fab2066b021
Dan Griffin <dangriffin13@gmail.com>
2017-07-13 03:13:06 -0400

    more formulas

--- model/mortgage_models.py
+++ model/mortgage_models.py
@@ -25,6 +25,12 @@ def payment_size(principal, rate, total_payments):
 	r = rate/12/100
 	n = total_payments
 
+	#(1+r)**-n discounts by rate r for n time periods
+	#(1 - (1 + r ** -n)) basically tells you how much you need to pay now to
+		# make up for the FV - PV diff at the current interest rate.
+		# In other words it's the present value of an interest payment
+		# you divide the principal by that so you can figure out the right size
+		# "hypothetical" principal needed to pay interest on to reach the future value
 	pmt = r * p / (1 - (1 + r)**-n)
 	return pmt
 
@@ -34,17 +40,21 @@ def interest_size(remaining_balance, rate):
 	return interest
 
 def principal_paydown(remaining_balance, payment, rate):
-	paydown = payment - (remaining_balance * rate)
-
+	paydown = payment - (remaining_balance * rate)	
+	return paydown
 
 def recalc_principal(remaining_balance, rate):
-	principal = remaining_balance - principal_paydown(remaining_balance, payment, rate)
+	remaining_balance -= remaining_balance - principal_paydown(remaining_balance, payment, rate)
+	return remaining_balance
 
-def recalc_cum_prin():
-	pass
+def recalc_cum_prin(prin_cum, remaining_balance, payment_period, rate):
+	prin_cum += principal_paydown(remaining_balance, payment_period, rate)
+	return prin_cum
 
-def recalc_cum_int():
-	pass
+def recalc_cum_int(int_cum, remaining_balance, rate):
+	int_cum += interest_size(remaining_balance, rate)
+	return int_cum
 
-def recalc_cum_pmt():
-	pass
+def recalc_cum_pmt(pmt_cum, principal, rate):
+	pmt_cum += principal * rate
+	return pmt_cum
